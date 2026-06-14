375ffff73a66a06a3d3a392cbf3a03364337be42
Gabriel <gabo.vm11@gmail.com>
2026-06-14 19:07:40 -0300

    Opçoes + limpada no codigo

--- src/z80/gui/gui.cpp
+++ src/z80/gui/gui.cpp
@@ -20,16 +20,13 @@
 #if defined(IMGUI_IMPL_OPENGL_ES2)
 #include <GLES2/gl2.h>
 #endif
-#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+#include <GLFW/glfw3.h> 
 
-// [Win32] Our example includes a copy of glfw3.lib pre-compiled with VS2010 to maximize ease of testing and compatibility with old VS compilers.
-// To link with VS2010-era libraries, VS2015+ requires linking with legacy_stdio_definitions.lib, which we do using this pragma.
-// Your own project should not be affected, as you are likely to link with a newer binary of GLFW that is adequate for your version of Visual Studio.
+
 #if defined(_MSC_VER) && (_MSC_VER >= 1900) && !defined(IMGUI_DISABLE_WIN32_FUNCTIONS)
 #pragma comment(lib, "legacy_stdio_definitions")
 #endif
 
-// This example can also compile and run with Emscripten! See 'Makefile.emscripten' for details.
 #ifdef __EMSCRIPTEN__
 #include "../libs/emscripten/emscripten_mainloop_stub.h"
 #endif
@@ -44,7 +41,7 @@ static MemoryEditor mem_edit;
 static AppLog log;
 
 
-// Main code
+// Codigo de Fato
 int GUI::run_interface(Memory &mem, CPU &cpu)
 {
 
@@ -52,7 +49,7 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
     if (!glfwInit())
         return 1;
 
-    // Decide GL+GLSL versions
+    // Definições do ImGui
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100 (WebGL 1.0)
     const char *glsl_version = "#version 100";
@@ -81,15 +78,15 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
     // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 
-    // Create window with graphics context
+    // Janela
     float main_scale = ImGui_ImplGlfw_GetContentScaleForMonitor(glfwGetPrimaryMonitor()); // Valid on GLFW 3.3+ only
     GLFWwindow *window = glfwCreateWindow((int)(1360 * main_scale), (int)(768 * main_scale), "Z80 Emulator", nullptr, nullptr);
     if (window == nullptr)
         return 1;
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(1); // Enable vsync
+    glfwSwapInterval(1); 
 
-    // Setup Dear ImGui context
+    // Setup ImGui 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO &io = ImGui::GetIO();
@@ -100,54 +97,30 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
     //  io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;   // Enable Multi-Viewport / Platform Windows
     //  io.ConfigViewportsNoAutoMerge = true;
     //  io.ConfigViewportsNoTaskBarIcon = true;
-
-    // Setup Dear ImGui style
+   
     ImGui::StyleColorsDark();
-    // ImGui::StyleColorsLight();
 
-    // Setup scaling
     ImGuiStyle &style = ImGui::GetStyle();
-    style.ScaleAllSizes(main_scale); // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-    style.FontScaleDpi = main_scale; // Set initial font scale. (in docking branch: using io.ConfigDpiScaleFonts=true automatically overrides this for every window depending on the current monitor)
+    style.ScaleAllSizes(main_scale); 
+    style.FontScaleDpi = main_scale; 
 #if GLFW_VERSION_MAJOR >= 3 && GLFW_VERSION_MINOR >= 3
-    io.ConfigDpiScaleFonts = true;     // [Experimental] Automatically overwrite style.FontScaleDpi in Begin() when Monitor DPI changes. This will scale fonts but _NOT_ scale sizes/padding for now.
-    io.ConfigDpiScaleViewports = true; // [Experimental] Scale Dear ImGui and Platform Windows when Monitor DPI changes.
+    io.ConfigDpiScaleFonts = true;     
+    io.ConfigDpiScaleViewports = true; 
 #endif
 
-    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
     if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
     {
         style.WindowRounding = 0.0f;
         style.Colors[ImGuiCol_WindowBg].w = 1.0f;
     }
 
-    // Setup Platform/Renderer backends
     ImGui_ImplGlfw_InitForOpenGL(window, true);
 #ifdef __EMSCRIPTEN__
     ImGui_ImplGlfw_InstallEmscriptenCallbacks(window, "#canvas");
 #endif
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    // Load Fonts
-    // - If fonts are not explicitly loaded, Dear ImGui will select an embedded font: either AddFontDefaultVector() or AddFontDefaultBitmap().
-    //   This selection is based on (style.FontSizeBase * style.FontScaleMain * style.FontScaleDpi) reaching a small threshold.
-    // - You can load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - If a file cannot be loaded, AddFont functions will return a nullptr. Please handle those errors in your code (e.g. use an assertion, display an error and quit).
-    // - Read 'docs/FONTS.md' for more instructions and details.
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use FreeType for higher quality font rendering.
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
-    // - Our Emscripten build process allows embedding fonts to be accessible at runtime from the "fonts/" folder. See Makefile.emscripten for details.
-    // style.FontSizeBase = 20.0f;
-    // io.Fonts->AddFontDefaultVector();
-    // io.Fonts->AddFontDefaultBitmap();
-    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf");
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf");
-    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf");
-    // ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
-    // IM_ASSERT(font != nullptr);
 
-    // Our state
     bool show_demo_window = false;
     static AppLog log;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
@@ -163,13 +136,14 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
                                       ImGuiWindowFlags_NoTitleBar |
                                       ImGuiWindowFlags_NoScrollbar;
 
-    // Main loop
+    
 #ifdef __EMSCRIPTEN__
     // For an Emscripten build we are disabling file-system access, so let's not attempt to do a fopen() of the imgui.ini file.
     // You may manually call LoadIniSettingsFromMemory() to load settings from your own storage.
     io.IniFilename = nullptr;
     EMSCRIPTEN_MAINLOOP_BEGIN
 #else
+    static bool running = false;
     while (!glfwWindowShouldClose(window))
 
 #endif
@@ -181,7 +155,8 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
             continue;
         }
         
-        if (!cpu.isHalted())
+
+        if (running && !cpu.isHalted())
         {
         cpu.step();
         }
@@ -198,12 +173,12 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
         float altura_topo = total_Y * 0.70f;
         float altura_fundo = total_Y * 0.30f;
 
-        // Start the Dear ImGui frame
+        // Começa o frame do ImGui
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        // 1. Janelas
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
 
@@ -310,6 +285,41 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
             regs.F.C
         );
 
+        ImGui::SeparatorText("Opções");
+
+        if (ImGui::Button("Run"))
+        {
+            running = true;
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Pause"))
+        {
+            running = false;
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Avançar p/ proxima instrução"))
+        {
+            running = false;
+
+            if (!cpu.isHalted())
+            {
+                cpu.step();
+            }
+        }
+
+        ImGui::SameLine();
+
+        if (ImGui::Button("Reset"))
+        {
+            running = false;
+            cpu.reset();
+        }
+        
+
         ImGui::End();
 
         // Editor de Memória (Lado Direito Inferior)
@@ -328,9 +338,6 @@ int GUI::run_interface(Memory &mem, CPU &cpu)
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-        // Update and Render additional Platform Windows
-        // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
-        //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
         {
             GLFWwindow *backup_current_context = glfwGetCurrentContext();
