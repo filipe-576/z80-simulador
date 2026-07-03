cd53921188afd9bf2218fd5e991bb921d3512869
gustavoskop <gkopereck@gmail.com>
2026-07-03 00:27:21 -0300

    docs(linker) adicionados comentários no header

--- src/linker/linker.h
+++ src/linker/linker.h
@@ -9,25 +9,35 @@ class Linker {
     public:
         Linker(const std::string& file);
         
+        /**
+         * @brief Executa o primeiro e o segundo passo do ligador
+         */
         void Link();
+
+        /**
+         * @brief Exibe o código de máquina após o segundo passo
+         */
         void linkDebug();
 
     private:
         struct Module {
             std::string name;
             int size;
-            std::unordered_map<std::string, int> definitionTable;
-            std::unordered_map<std::string, std::vector<int>> useTable; 
+            std::unordered_map<std::string, int> definitionTable; //INTDEF
+            std::unordered_map<std::string, std::vector<int>> useTable; //INTUSE
             std::vector<int> machineCode;
             std::vector<int> realocationMap; 
         };
 
         std::vector<Module> listModules;
-        std::unordered_map<std::string, int> globalSymbolTable; 
+        std::unordered_map<std::string, int> globalSymbolTable; // TSG
         std::unordered_map<std::string, int> modulesOffsets; 
 
         void oPrimeiroPassoDosLinkerBoys();
         void oSegundoPassoDosLinkerBoys();
 
+        /**
+         * @brief Lê a saída do assembler, um json com informações dos códigos a serem ligados
+         */
         void loadJson(const std::string& fileName); 
 }; 
--- src/linker/main.cpp
+++ src/linker/main.cpp
@@ -7,7 +7,7 @@
 #endif
 
 int main() {
-    // Monta o caminho absoluto: "C:/.../projeto" + "/src/linker/modules.json"
+
     std::string jsonPath = std::string(PROJ_DIR) + "/src/linker/modules.json";
     Linker linker(jsonPath);
     linker.Link();
