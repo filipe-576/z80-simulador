ed07fec1f46049086f0dfc273cebb91fd82fe113
filipe-576 <filipe.contato99@gmail.com>
2026-07-10 19:33:53 -0300

    feat(assembler) implementado leitura de linha de comando para arquivos de entrada e saída

--- src/assembler/main.cpp
+++ src/assembler/main.cpp
@@ -1,11 +1,37 @@
 #include "assembler.h"
-#ifndef PROJ_DIR
-#define PROJ_DIR "."
-#endif
+#include <iostream>
+#include <string>
 
 
-int main(){
-    Assembler assemb = Assembler(std::string(PROJ_DIR) + "/src/assembler/test2.asm");
+static void invalidCommand() {
+    std::cerr << "Uso: ./assembler <arquivo.asm> [-o <saida>]\n";
+}
+
+int main(int argc, char* argv[]) {
+    std::string input_file;
+    std::string output_file = "a.o";
+
+    for ( int i = 1; i < argc; ++i ) {
+        std::string arg = argv[i];
+        if ( arg == "-o" ) {
+            if( i+1 > argc ) invalidCommand();
+            output_file = argv[++i];
+        } else if ( input_file.empty() ) {
+            input_file = arg;
+        } else {
+            invalidCommand();
+            return 1;
+        }
+    }
+
+    if ( input_file.empty() ) {
+        invalidCommand();
+        return 1;
+    }
+
+    Assembler assemb(input_file, output_file);
     assemb.assemble();
-    assemb.debug();
+    // assemb.debug();
+
+    return 0;
 }
