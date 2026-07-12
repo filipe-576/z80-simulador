d497c1f5152e6bc4d2a5c28bf10509294b4feade
filipe-576 <filipe.contato99@gmail.com>
2026-07-11 22:16:22 -0300

    feat(macro) implementa definição de arquivo de entrada e saída por terminal

---
+++ src/macro/main.cpp
@@ -0,0 +1,34 @@
+#include "macro.h"
+#include <iostream>
+
+static void invalidCommand() {
+    std::cerr << "Uso: ./assembler <arquivo.asm> [-o <saida>]\n";
+}
+
+int main(int argc, char* argv[]) {
+    std::string inputFile;
+    std::string outputFile = "a.o";
+
+    for ( int i = 1; i < argc; ++i ) {
+        std::string arg = argv[i];
+        if ( arg == "-o" ) {
+            if( i+1 > argc ) invalidCommand();
+            outputFile = argv[++i];
+        } else if ( inputFile.empty() ) {
+            inputFile = arg;
+        } else {
+            invalidCommand();
+            return 1;
+        }
+    }
+
+    if ( inputFile.empty() ) {
+        invalidCommand();
+        return 1;
+    }
+
+    MacroProcessor macroProcessor(inputFile, outputFile);
+    macroProcessor.process();
+
+    return 0;
+}
